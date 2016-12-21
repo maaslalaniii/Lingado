6b521d364fecd392469bec55d12d1677eced693f
Maas Lalani <maas@live.ca>
2016-12-20 20:32:55 -0500

    Fixed indentation issues.

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Text, Image, ToolbarAndroid, TouchableNativeFeedback, UIManager, findNodeHandle } from 'react-native'
 
+import firebase from '../modules/firebase'
 
 export default class Home extends Component {
-	render() {
-		return (
-			<View>
-			</View>
-		)
-	}
+
+  _showMenu() {
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.toolbar}>
+          <Text style={styles.title}>Lingado</Text>
+          <TouchableNativeFeedback onPress={this._showMenu} background={TouchableNativeFeedback.Ripple('white', true)}>
+            <View>
+              <Image style={styles.moreVert} source={require('../images/icons/more_vert.png')} />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      </View>
+    )
+  }
+  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#203040'
+  },
+  toolbar: {
+    height: 56,
+    backgroundColor: '#34495e',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 10
+  },
+  title: {
+    color: 'white',
+    fontSize: 22,
+    marginLeft: 10
+  },
+  moreVert: {
+    height: 28,
+    width: 28,
+  }
+})
--- app/screens/index.js
+++ app/screens/index.js
@@ -7,41 +7,41 @@ import Home from './home'
 import Settings from './settings'
 
 const routes = [
-	{ title: 'Login', index: 0 },
-	{ title: 'Setup', index: 1 },
-	{ title: 'Home', index: 2 },
-	{ title: 'Settings', index: 3 }
+  { title: 'Login', index: 0 },
+  { title: 'Setup', index: 1 },
+  { title: 'Home', index: 2 },
+  { title: 'Settings', index: 3 }
 ]
 
 export default class App extends Component {
-	_renderScene(route, navigator) {
-		switch (route.title) {
-			case 'Login':
-				return <Login routes={routes} navigator={navigator} />
-				break
+  _renderScene(route, navigator) {
+    switch (route.title) {
+      case 'Login':
+        return <Login routes={routes} navigator={navigator} />
+        break
 
-			case 'Setup':
-				return <Setup routes={routes} navigator={navigator} />
-				break
+      case 'Setup':
+        return <Setup routes={routes} navigator={navigator} />
+        break
 
-			case 'Home':
-				return <Home routes={routes} navigator={navigator} />
-				break
+      case 'Home':
+        return <Home routes={routes} navigator={navigator} />
+        break
 
-			case 'Settings':
-				return <Settings routes={routes} navigator={navigator} />
-				break
-		}
-	}
+      case 'Settings':
+        return <Settings routes={routes} navigator={navigator} />
+        break
+    }
+  }
 
-	render() {
-		return (
-			<Navigator
-				configureScene={() => Navigator.SceneConfigs.PushFromRight}
-				initialRoute={routes[0]}
-				initialRouteStack={routes}
-				renderScene={this._renderScene.bind(this)}
-				/>
-		)
-	}
+  render() {
+    return (
+      <Navigator
+        configureScene={() => Navigator.SceneConfigs.PushFromRight}
+        initialRoute={routes[0]}
+        initialRouteStack={routes}
+        renderScene={this._renderScene.bind(this)}
+        />
+    )
+  }
 }
