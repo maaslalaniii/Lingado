2cc038413acdf9cd62492f1d38183c02838f6eda
Shehryar Assad <xshehryar@gmail.com>
2017-01-31 21:37:14 -0500

    Create QRCodeScanner.js

---
+++ QRCodeScanner.js
@@ -0,0 +1,23 @@
+use 'strict';
+var React = require('react-native');
+
+var{
+  Text,
+  View,
+  StyleSheet,
+} = React;
+
+// make sure you linked rights + added camera modules to src/ before continuing!
+import Camera from 'react-native-camera';
+var QRCodeRead = React.createClass({
+
+
+)};
+
+
+// Make a cancel button, a square and an opacity layer using StyleSheet
+// ...
+// add code
+
+module.exports = QRCodeRead;
+
