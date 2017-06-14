40a760536e905bc11fae78b5999eee32eb18d447
Maas Lalani <maas@live.ca>
2017-06-14 17:15:58 -0400

    Removed the routes stack

--- main.js
+++ main.js
@@ -4,22 +4,16 @@ import Home from './app/Screens/Home'
 import Setup from './app/Screens/Setup'
 import Expo from 'expo'
 
-const routes = [
-  { title: 'Setup', index: 0 },
-  { title: 'Home', index: 1 },
-]
-
-
 class App extends React.Component {
   _renderScene(route, navigator) {
     switch(route.title) {
 
       case 'Setup':
-        return <Setup routes={routes} navigator={navigator} />
+        return <Setup navigator={navigator} />
         break
 
       case 'Home':
-        return <Home routes={routes} navigator={navigator} />
+        return <Home navigator={navigator} code={route.code} />
         break
     }
   }
